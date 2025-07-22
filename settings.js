93069dd990b0058dcdca13c77e25f0a62d5eb0b7
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-07-22 09:53:46 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //BOT SETTINGS//
-global.xprefix = '.'
+global.xprefix = ','
 global.ownername = 'ElitePro'
 global.botname = 'ELITE-PRO-V2'
 global.ytname = "YT: EliteProTechs"
 global.socialm = "GitHub: EliteProTech"
-global.location = "Port Harcourt, Nigeria"
-global.ownernumber = '2348109263390'
+global.location = "Johannesburg, South Africa"
+global.ownernumber = '27634624586'
 
 //BOT SETTINGS 2//
-global.premium = ["2348109263390"] //Premium 
+global.premium = ["27634624586"] //Premium 
 global.websitex = "https://youtube.com/@eliteprotechs"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
 global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/EliteProTech/Elite-Pro-V2'
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "Tuesday/Tristan\n\n+27634624586"
+global.creator = "27634624586@s.whatsapp.net"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg'
 
 //DEFAULT SETTINGS//
@@ -31,9 +31,9 @@ global.antiforeignnumber = '91' //anti foreign number
 // TOGGLE CMDS: Enable=true Disable=false
 global.welcome = false //welcome/left in groups
 global.anticall = false //blocks user that call
-global.autoswview = false // view status
-global.adminevent = false //show promote/demote mess
-global.groupevent = false //show update messages in group chat
+global.autoswview = true // view status
+global.adminevent = true //show promote/demote mess
+global.groupevent = true //show update messages in group chat
 global.autoreact = false //React to messages
 
 //DEFAULT SETTINGS 3//
